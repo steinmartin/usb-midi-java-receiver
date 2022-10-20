91c78c9180ddd51089fd07121908029b4f8bd7d6
Martin Stein <m.stein@gsi.de>
2022-10-20 16:28:27 +0200

    First version of conversion of timestamp from six 7-bit large bytes to a Java long variable

--- src/main/java/org/example/MidiInputReceiver.java
+++ src/main/java/org/example/MidiInputReceiver.java
@@ -13,41 +13,42 @@ public class MidiInputReceiver implements Receiver {
 
     @Override
     public void send(MidiMessage message, long timeStamp) {
-        System.out.println("midi received");
         if (message instanceof SysexMessage) {
-            System.out.println("Received SysexMessage");
+            System.out.println("Received SysexMessage:");
             SysexMessage sysexMessage = (SysexMessage) message;
-//            System.out.print(Byte.toString(sysexMessage.getData()[0]));
-//            System.out.print(Integer.toHexString(sysexMessage.getData()[1]));
-//            System.out.print(Integer.toHexString(sysexMessage.getData()[2]));
-//            System.out.print(Integer.toHexString(sysexMessage.getData()[3]));
-//            System.out.print(Integer.toHexString(sysexMessage.getData()[4]));
-//            System.out.print(Integer.toHexString(sysexMessage.getData()[5]));
-//            System.out.println(Integer.toHexString(sysexMessage.getData()[6]));
-//
-            System.out.print(sysexMessage.getData()[0]);
-            System.out.print(' ');
-            System.out.print(sysexMessage.getData()[1]);
-            System.out.print(' ');
-            System.out.print(sysexMessage.getData()[2]);
-            System.out.print(' ');
-            System.out.print(sysexMessage.getData()[3]);
-            System.out.print(' ');
-            System.out.print(sysexMessage.getData()[4]);
-            System.out.print(' ');
-            System.out.print(sysexMessage.getData()[5]);
-            System.out.print(' ');
-            System.out.print(sysexMessage.getData()[6]);
-            System.out.print(' ');
-            System.out.print(sysexMessage.getData()[7]);
-            System.out.print(' ');
+
+            System.out.print(sysexMessage.getData()[0] + " ");
+            System.out.print(sysexMessage.getData()[1] + " ");
+            System.out.print(sysexMessage.getData()[2] + " ");
+            System.out.print(sysexMessage.getData()[3] + " ");
+            System.out.print(sysexMessage.getData()[4] + " ");
+            System.out.print(sysexMessage.getData()[5] + " ");
+            System.out.print(sysexMessage.getData()[6] + " ");
+            System.out.print(sysexMessage.getData()[7] + " ");
             System.out.println(sysexMessage.getData()[8]);
 
-            //System.out.print(Integer.toHexString(sysexMessage.getData()[1]));
-            //System.out.println(Integer.toHexString(sysexMessage.getData()[2]));
-            System.out.println("Increment: " + combine(sysexMessage.getData()[1], sysexMessage.getData()[2]));
-            System.out.println("Timestamp in microseconds: " + combine(sysexMessage.getData()[3], sysexMessage.getData()[4],
-                    sysexMessage.getData()[5], sysexMessage.getData()[6], sysexMessage.getData()[7], sysexMessage.getData()[8]));
+            long microsecondsSinceMidnight = combine(sysexMessage.getData()[1], sysexMessage.getData()[2],
+                    sysexMessage.getData()[3], sysexMessage.getData()[4], sysexMessage.getData()[5], sysexMessage.getData()[6]);
+
+            // binary representation of the 6 timestamp bytes
+//            binaerDarstellenVonByte ("", sysexMessage.getData()[1]);
+//            System.out.print(" ");
+//            binaerDarstellenVonByte ("", sysexMessage.getData()[2]);
+//            System.out.print(" ");
+//            binaerDarstellenVonByte ("", sysexMessage.getData()[3]);
+//            System.out.print(" ");
+//            binaerDarstellenVonByte ("", sysexMessage.getData()[4]);
+//            System.out.print(" ");
+//            binaerDarstellenVonByte ("", sysexMessage.getData()[5]);
+//            System.out.print(" ");
+//            binaerDarstellenVonByte ("", sysexMessage.getData()[6]);
+//            System.out.println();
+
+            System.out.println("Increment: " + combine(sysexMessage.getData()[7], sysexMessage.getData()[8]));
+            //binaerDarstellenVonLong ("", microsecondsSinceMidnight);
+
+            System.out.println("Timestamp in microseconds: " + microsecondsSinceMidnight);
+            System.out.println();
         }
     }
 
@@ -66,15 +67,58 @@ public class MidiInputReceiver implements Receiver {
     long combine(byte b1, byte b2, byte b3, byte b4, byte b5, byte b6) {
 
         long res = 0;
-        //res = (b1 << 35) | (b2 << 28) | (b3 << 21) | (b4 << 14) | (b5 << 7) | b6;
-        res = b6 | (b5 << 7) | (b4 << 14) | (b3 << 21) | (b2 << 28) | (b1 << 35);
+        res = (long) b1 << 35;
+        res = (res | (long) b2 << 28);
+        res = (res | (long) b3 << 21);
+        res = (res | (long) b4 << 14);
+        res = (res | (long) b5 << 7);
+        res = (res | (long) b6);
 
-//        res = b6 | (b5 << 7);
-//        res = b5 | (b4 << 14);
-//        res = b4 | (b3 << 21);
-//        res = b3 | (b2 << 28);
-//        res = b2 | (b1 << 35);
-        res = res << 21 >> 21;  // sign-extend as 32 bit
         return res;
+    }
+
+    public static void binaerDarstellenVonByte (String text, byte zahl) {
+
+        byte maske = 0b00000001;
+        char[] bitfolge = new char[8];
+
+        for (int i = 0; i < 8; i++)
+        {
+            bitfolge[7 - i] = (zahl & maske) == 0 ? '0' : '1';
+            maske = (byte) (maske << 1);
+        }
+
+        System.out.print(text);
+        System.out.print(bitfolge);
+    }
+
+
+    public static void binaerDarstellenVonLong (String text, long zahl) {
+
+        long maske = 0b0000000000000000000000000000000000000000000000000000000000000001;
+        char[] bitfolge = new char[64];
+
+        for (int i = 0; i < 64; i++)
+        {
+            bitfolge[63 - i] = (zahl & maske) == 0 ? '0' : '1';
+            maske = maske << 1;
+        }
+
+        System.out.print(text);
+        System.out.println(bitfolge);
+    }
+    public static void binaerDarstellenVonInt (String text, int zahl) {
+
+        int maske = 0b00000000000000000000000000000001;
+        char[] bitfolge = new char[32];
+
+        for (int i = 0; i < 32; i++)
+        {
+            bitfolge[31 - i] = (zahl & maske) == 0 ? '0' : '1';
+            maske = maske << 1;
+        }
+
+        System.out.print(text);
+        System.out.println(bitfolge);
     }
 }
